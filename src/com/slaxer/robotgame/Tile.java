0e733d9c63f90eb15ad39ab78dc2719cee895997
JK Wood <joshuakwood@gmail.com>
2013-03-03 17:45:54 -0600

    Port Tile class to Kilobolt Android Game Framework

--- src/com/slaxer/robotgame/Tile.java
+++ src/com/slaxer/robotgame/Tile.java
@@ -1,15 +1,17 @@
 package com.slaxer.robotgame;
 
-import java.awt.Image;
-import java.awt.Rectangle;
+import android.graphics.Rect;
+
+import com.slaxer.framework.Image;
 
 public class Tile {
-	private int tileX, tileY, speedX, type;
+	private int tileX, tileY, speedX;
+	public int type;
 	public Image tileImage;
-	private Rectangle collRect;
+	private Rect collRect;
 
-	private Robot robot = StartingClass.getRobot();
-	private Background bg = StartingClass.getBg1();
+	private Robot robot = GameScreen.getRobot();
+	private Background bg = GameScreen.getBg1();
 
 	public Tile(int x, int y, int typeInt) {
 		// Why 40? Each tile occupies 40 pixels.
@@ -17,19 +19,19 @@ public class Tile {
 		tileY = y * 40;
 
 		type = typeInt;
-		
-		collRect = new Rectangle();
+
+		collRect = new Rect();
 
 		if (type == 5)
-			tileImage = StartingClass.tiledirt;
+			tileImage = Assets.tiledirt;
 		else if (type == 8)
-			tileImage = StartingClass.tilegrassTop;
+			tileImage = Assets.tilegrassTop;
 		else if (type == 4)
-			tileImage = StartingClass.tilegrassLeft;
+			tileImage = Assets.tilegrassLeft;
 		else if (type == 6)
-			tileImage = StartingClass.tilegrassRight;
+			tileImage = Assets.tilegrassRight;
 		else if (type == 2)
-			tileImage = StartingClass.tilegrassBot;
+			tileImage = Assets.tilegrassBottom;
 		else
 			type = 0;
 	}
@@ -38,11 +40,12 @@ public class Tile {
 	public void update() {
 		speedX = bg.getSpeedX() * 5;
 		tileX += speedX;
-		collRect.setBounds(tileX, tileY, 40, 40);
-		
-		if(collRect.intersects(Robot.robotAreaRectangle) && type != 0){
-			checkVerticalCollision(Robot.upperCollRect,Robot.lowerCollRect);
-			checkSideCollision(Robot.leftCollRect, Robot.rightCollRect, Robot.leftFootCollRect, robot.rightFootCollRect);
+		collRect.set(tileX, tileY, 40, 40);
+
+		if (Rect.intersects(collRect, Robot.robotAreaRect) && type != 0) {
+			checkVerticalCollision(Robot.upperCollRect, Robot.lowerCollRect);
+			checkSideCollision(Robot.leftCollRect, Robot.rightCollRect,
+					Robot.leftFootCollRect, robot.rightFootCollRect);
 		}
 	}
 
@@ -69,43 +72,47 @@ public class Tile {
 	public void setTileImage(Image tileImage) {
 		this.tileImage = tileImage;
 	}
-	
+
 	// This prevents us from dropping through platforms or the ground.
-	public void checkVerticalCollision(Rectangle robotUpperCollRect, Rectangle robotLowerCollRect){
-		if(robotUpperCollRect.intersects(collRect)){
+	public void checkVerticalCollision(Rect robotUpperCollRect,
+			Rect robotLowerCollRect) {
+		if (Rect.intersects(collRect, robotUpperCollRect)) {
 			// DEBUG Fix this later
 		}
 		// Stop moving if the robot runs into a dirt tile
-		if(robotLowerCollRect.intersects(collRect) && type == 8){
+		if (Rect.intersects(collRect, robotLowerCollRect) && type == 8) {
 			robot.setJumped(false);
 			robot.setSpeedY(0);
 			robot.setCenterY(tileY - 63);
 		}
 	}
-	
+
 	// This prevents us from running through tiles.
-	public void checkSideCollision(Rectangle robotLeftCollRect, Rectangle robotRightCollRect, Rectangle robotLeftFootCollRect, Rectangle robotRightFootCollRect){
-		// Don't check side collisions with dirt, grass on the bottom of the tile, or empty tiles
-		if(type != 5 && type != 2 && type != 0){
-			// Check if we've hit on the left 
-			if(robotLeftCollRect.intersects(collRect)){
+	public void checkSideCollision(Rect robotLeftCollRect,
+			Rect robotRightCollRect, Rect robotLeftFootCollRect,
+			Rect robotRightFootCollRect) {
+		// Don't check side collisions with dirt, grass on the bottom of the
+		// tile, or empty tiles
+		if (type != 5 && type != 2 && type != 0) {
+			// Check if we've hit on the left
+			if (Rect.intersects(collRect, robotLeftCollRect)) {
 				// Move the bot outside of the collision rectangle and stop it.
 				robot.setCenterX(tileX + 102);
 				robot.setSpeedX(0);
-			// Check if we've hit on the left foot
-			} else if (robotLeftFootCollRect.intersects(collRect)){
+				// Check if we've hit on the left foot
+			} else if (Rect.intersects(collRect, robotLeftFootCollRect)) {
 				// Move the bot outside of the collision rectangle and stop it.
 				robot.setCenterX(tileX + 85);
 				robot.setSpeedX(0);
 			}
-			
+
 			// Check if we've hit on the right
-			if(robotRightCollRect.intersects(collRect)){
+			if (Rect.intersects(collRect, robotRightCollRect)) {
 				// Move the bot outside of the collision rectangle and stop it.
 				robot.setCenterX(tileX - 62);
 				robot.setSpeedX(0);
-			// Check if we've hit on the right foot
-			} else if (robotRightFootCollRect.intersects(collRect)){
+				// Check if we've hit on the right foot
+			} else if (Rect.intersects(collRect, robotRightFootCollRect)) {
 				// Move the bot outside of the collision rectangle and stop it.
 				robot.setCenterX(tileX - 45);
 				robot.setSpeedX(0);
