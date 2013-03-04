fea48d0fe1a13fd0e7942f69a334cc62fedb8287
JK Wood <joshuakwood@gmail.com>
2013-03-03 20:43:47 -0600

    Correct invalid asset paths in LoadingScreen class

--- src/com/slaxer/robotgame/LoadingScreen.java
+++ src/com/slaxer/robotgame/LoadingScreen.java
@@ -19,8 +19,8 @@ public class LoadingScreen extends Screen {
 		Assets.character = g.newImage("character.png", ImageFormat.ARGB4444);
 		Assets.character2 = g.newImage("character2.png", ImageFormat.ARGB4444);
 		Assets.character3 = g.newImage("character3.png", ImageFormat.ARGB4444);
-		Assets.characterDown = g.newImage("characterDown.png", ImageFormat.ARGB4444);
-		Assets.characterJump = g.newImage("characterJump.png", ImageFormat.ARGB4444);
+		Assets.characterDown = g.newImage("down.png", ImageFormat.ARGB4444);
+		Assets.characterJump = g.newImage("jumped.png", ImageFormat.ARGB4444);
 		
 		Assets.heliboy = g.newImage("heliboy.png", ImageFormat.ARGB4444);
 		Assets.heliboy2 = g.newImage("heliboy2.png", ImageFormat.ARGB4444);
@@ -29,12 +29,12 @@ public class LoadingScreen extends Screen {
 		Assets.heliboy5 = g.newImage("heliboy5.png", ImageFormat.ARGB4444);
 		
 		Assets.tiledirt = g.newImage("tiledirt.png", ImageFormat.RGB565);
-		Assets.tilegrassTop = g.newImage("tilegrassTop.png", ImageFormat.RGB565);
-		Assets.tilegrassBottom = g.newImage("tilegrassBot.png", ImageFormat.RGB565);
-		Assets.tilegrassLeft = g.newImage("tilegrassLeft.png", ImageFormat.RGB565);
-		Assets.tilegrassRight = g.newImage("tilegrassRight.png", ImageFormat.RGB565);
+		Assets.tilegrassTop = g.newImage("tilegrasstop.png", ImageFormat.RGB565);
+		Assets.tilegrassBottom = g.newImage("tilegrassbot.png", ImageFormat.RGB565);
+		Assets.tilegrassLeft = g.newImage("tilegrassleft.png", ImageFormat.RGB565);
+		Assets.tilegrassRight = g.newImage("tilegrassright.png", ImageFormat.RGB565);
 		
-		Assets.button = g.newImage("button.png", ImageFormat.RGB565);
+		Assets.button = g.newImage("button.jpg", ImageFormat.RGB565);
 		
 		game.setScreen(new MainMenuScreen(game));
 	}
